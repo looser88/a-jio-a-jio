c360f2e12d59026c0ab96c87b8e5a6011eb8b9a7
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-07-21 21:38:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -99,8 +99,9 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo="https://graph.org/file/983729d2228bafb4aad71.jpg",
+            caption=START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
