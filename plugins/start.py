3cc634979b052ff21448621db00eb12209d10939
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-05 14:13:48 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,7 +14,7 @@ from bot import Bot
 from config import LOG_ID, LOG_TEXT_P, ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-from datetime import datetime, timedelta
+import datetime 
 
 
 
