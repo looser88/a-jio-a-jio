34fe5e9523e036780822638976faacd1b6b6e4fa
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-05 19:35:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, LOG_ID, LOG_TEXT_P
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-from datetime
+import datetime
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
