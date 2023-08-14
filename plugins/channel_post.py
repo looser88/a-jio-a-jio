44eaa2749a8a432c4f4791cb602f164182d5db44
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-14 19:38:17 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -67,14 +67,17 @@ async def channel_post(client: Client, message: Message):
     Slink = await get_short(SL_URL, SL_API, Tlink) #generating short link with particular domine and api
     await bot_msg.edit("Analysing....!")
     await asyncio.sleep(1)
-    await bot_msg.edit("Wait Sending Photo ▣ ▢ ▢ ")
+    await bot_msg.edit("Wait Sending Post ▣ ▢ ▢ ")
     await asyncio.sleep(1)
     await bot_msg.edit("Wait Sending Photo ▣ ▣ ▢ ")
     await asyncio.sleep(1)
     await bot_msg.edit("Wait Sending Photo ▣ ▣ ▣ ")
     await asyncio.sleep(1)
-    await client.send_photo(chat_id=chtid, photo=pic, caption=FOMET.format(DATEDAY[-1], Slink, Slink))
-    await asyncio.sleep(1)
+    if len(DATEDAY)==0:
+        await bot_msg.edit("Error: invalid date please set /date")
+    else:
+        await client.send_photo(chat_id=chtid, photo=pic, caption=FOMET.format(botfsno, DATEDAY[-1], Slink, Slink))
+        await asyncio.sleep(1)
     await bot_msg.edit(BOTEFITMSG.format(filname, botfsno[0], Tlink, Slink, DATEDAY[-1])) #msg edit to "please wait...(see line 39" msg ==> and finally the elements belongs to sent serials are updated here
     #await e_pic.edit) # msg edit in forwarder channel = "pic without captions (see line 41)" ==> thats return to our given format and short link ,date are updated here
 
