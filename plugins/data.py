58e28434306abde836167d8a6063fb36630a6637
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-14 08:19:04 +0530

    Create data.py

---
+++ plugins/data.py
@@ -0,0 +1,141 @@
+FOMET = """
+▬▬▬▬▬▬▬ ❂ ▬▬▬▬▬▬▬
+       
+ 🗓𝐃𝐚𝐭𝐞 :- <b>{}</b>
+
+            𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐛𝐲 :- @Dot_serials_bot 
+
+                             ⚜⚜⚜⚜⚜     
+ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :-
+{}
+{}
+
+  👇👇 𝐇𝐨𝐰 𝐭𝐨 𝐨𝐩𝐞𝐧 𝐥𝐢𝐧𝐤👇👇
+https://t.me/+Sb5ro1gyhgY0NWM1
+https://t.me/+Sb5ro1gyhgY0NWM1
+  """
+BOTEFITMSG = """<b>
+Message Successfully sent ☺️
+Elements in "{}{}"
+1.Telegram Link: <a href="{}">Tlink</a>
+2.Short Link: <a href="{}">SLink</a>
+3.Date : {} 
+</b>"""
+DATAODD = {
+        'Trinayani_':['https://graph.org/file/1f2827a0980cc418ee6e1.jpg','tnvalue.in','ff426552eda72230153ea3450a0bce0557183ccb','-1001748750847'],
+        'Annapoorna_':['https://graph.org/file/baff21a920cc09f0ba89a.jpg','moneycase.link','0c2406a0cf1b54db75fcba4cbb8e389bdc675eee','-1001748750847'],
+        'Kalyanamasthu_':['https://graph.org/file/db7a3d7d1a2654540cf99.jpg','','',''],
+        'Vaidehi_Parinaya_':['https://graph.org/file/8c7eda434e1f0baae0b81.jpg','','',''],
+        'Punarvivaha_':['https://graph.org/file/70d5850fd6e9ed8cd7bc0.jpg','','',''],
+        'Paaru_':['https://graph.org/file/f4eccb711889b4e33952a.jpg','tnvalue.in','e496ab76b42f51628da20bb69577397533008f3c','-1001748750847'],
+        'Bhoomige_Bandha_Bhagavantha_':['https://graph.org/file/dc3b179fceb487e4847c4.jpg','moneycase.link','bdc62bf7dd54515abf15371b58feb6a1ff2b434a','-1001748750847'],
+        'Puttakkana_Makkalu_':['https://graph.org/file/085afbc67400a2066fefd.jpg','','',''],
+        'Gattimela_':['https://graph.org/file/d3eda0e1961ee06e5cba7.jpg','','',''],
+        'Manemagalu_':['','','',''],
+        'Sandhyaraaga_':['https://graph.org/file/98ef89a04dd66bef924ea.jpg','','',''],
+        'SeethaRaama_':['https://graph.org/file/d7b3c98a89d0d061f064f.jpg','','',''],
+        'Amruthadhaare_':['https://graph.org/file/2ebe4f1d999849a1ca574.jpg','','',''],
+        'Mahanayaka_Dr_B_R_Ambedkar_':['https://graph.org/file/6faf520d534a49f6742bb.jpg','','',''],
+        'Shrirasthu_Shubhamasthu_':['https://graph.org/file/b6d93c7cc15166fbf7e35.jpg','moneycase.link','f35b0c3ac535a200b3287c698b652e58b4689b6f','-1001748750847'],
+        'Soubhagyavati_Bhava_':['https://graph.org/file/2d34fb7705e6ecb7c5937.jpg','tnvalue.in','8fc4e5a5a7f1d571adfc7c02537d7c1e73da1a15','-1001748750847'],
+        'Hitler_Kalyana_':['https://graph.org/file/4bccb3c8fd1fe4c5d92e7.jpg','','',''],
+        'Sathya_':['https://graph.org/file/7f30ff54ecf9b380e4634.jpg','','',''],
+        'Dance_Karnataka_Dance_Season_7_':['https://graph.org/file/2d724f7a19f736c405990.jpg','','',''],
+        'Bharjari_Bachelors_':['https://graph.org/file/1b28fb7863cc31f732ac4.jpg','','',''],
+        'Couples_Kitchen_':['https://graph.org/file/fa897d25d4d944e8487d5.jpg','','',''],
+        'Chota_Champion_':['https://graph.org/file/0d77fc85da98174336f8b.jpg','','',''],
+          
+        'Pavada_Purasha_':['https://graph.org/file/f65651907ba441187f7b1.jpg','tnvalue.in','ff426552eda72230153ea3450a0bce0557183ccb','-1001748750847'],
+        'Olavina_Nildana_':['https://graph.org/file/441e02c5e145609e0eedd.jpg','','',''],
+        'Kendasampige_':['https://graph.org/file/62c90177d09f7cf416e6c.jpg','','',''],
+        'Bhagyalakshmi_':['https://graph.org/file/261d0c995b5e2a434093b.jpg','','',''],
+        'Ramachari_':['https://graph.org/file/f590c3585b4c0068df630.jpg','','',''],
+        'Lakshmi_Baramma_':['https://graph.org/file/be3588e70978523792514.jpg','','',''],
+        'Geetha_':['https://graph.org/file/e95baeeafbf180bd184e1.jpg','','',''],
+        'Lakshana_':['https://graph.org/file/995a7a33f5d543f4f4dbf.jpg','','',''],
+        'Punyavathi_':['https://graph.org/file/c15998a362846c9eb09f0.jpg','','',''],
+        'Tripura_Sundari_':['https://graph.org/file/4a59fd893093b82e9c234.jpg','tnvalue.in','8fc4e5a5a7f1d571adfc7c02537d7c1e73da1a15','-1001748750847'],
+        'Antarapata_':['https://graph.org/file/ea08c222d4848f1661e3c.jpg','','',''],
+        'Gruhapravesha_':['https://graph.org/file/16220456e337fa35b18b7.jpg','tnvalue.in','e496ab76b42f51628da20bb69577397533008f3c','-1001748750847'],
+        'Ganda_Hendthi_':['https://graph.org/file/e7437d0a9eac55fdb2623.jpg','','',''],
+        'Shantham_Papam_':['https://graph.org/file/fa897d25d4d944e8487d5.jpg','','',''],
+        'Muddu_Bangara_':['https://graph.org/file/49001901d7ccbf8698935.jpg','','',''],
+        'Gunasundari_':['https://graph.org/file/8d4b6b2e9703a7d97b546.jpg','','',''],
+        'Anu_':['https://graph.org/file/7a8f5ce25159932859193.jpg','','',''],
+        'Family_Gangstars_':['https://graph.org/file/d08c55a1b259a3b5c95b7.jpg','','',''],
+
+        'Neenadhe_Naa_':['https://graph.org/file/fcbbf0a8c0413d4a59ad9.jpg','','',''],
+        'Namma_Lachi_':['https://graph.org/file/b315e59c10e04d1b2e595.jpg','','',''],
+        'Nagapanchami_':['https://graph.org/file/dd2a58d339ca517010edc.jpg','','',''],
+        'Raani_':['https://graph.org/file/5a8aed0bcd6e14604b5d8.jpg','','',''],
+        'Katheyondu_Shuruvagide_':['https://graph.org/file/e9b9f1a3dd7bc26772d5c.jpg','tnvalue.in','e496ab76b42f51628da20bb69577397533008f3c',''],
+        'O_MuddhuManase_':['https://graph.org/file/cd2664a143bec7e2d5f14.jpg','','',''],
+        'Jenugudu_':['https://graph.org/file/48a8df66387287f6e2440.jpg','','',''],
+        'Udho_Udho_Shri_Renuka_Yellamma_':['https://graph.org/file/f5749c1343173f855b0e7.jpg','','',''],
+        'Aragini_2_':['https://graph.org/file/6ddd25521aa2c813306aa.jpg','','',''],
+        'Honganasu_':['https://graph.org/file/ca9bcfcfb5560459c3bcf.jpg','tnvalue.in','8fc4e5a5a7f1d571adfc7c02537d7c1e73da1a15',''],
+        'Anuraga_Aralitu_':['https://graph.org/file/0b15dd8c7f1d0118af62e.jpg','tnvalue.in','ff426552eda72230153ea3450a0bce0557183ccb',''],
+        'Anupama_Sarvagunaa_Sampanne_':['https://graph.org/file/0e86ebb3ab5cf24eac6c9.jpg','','',''],
+        'Yediyur_Shree_Siddhalingeshwara_':['https://graph.org/file/20ff06892579f4e3ac9ed.jpg','','',''],
+        'Bombat_Bhojana_':['https://graph.org/file/d2871411e7358e4c76b52.jpg','','',''],
+        'Suvarna_Superstar_':['https://graph.org/file/09f90f559d100a415f393.jpg','','','']
+}
+DATAEVEN = {
+          'Trinayani_':['https://graph.org/file/1f2827a0980cc418ee6e1.jpg','moneycase.link','0c2406a0cf1b54db75fcba4cbb8e389bdc675eee','1001748750847'],
+        'Annapoorna_':['https://graph.org/file/baff21a920cc09f0ba89a.jpg','','',''],
+        'Kalyanamasthu_':['https://graph.org/file/db7a3d7d1a2654540cf99.jpg','','',''],
+        'Vaidehi_Parinaya_':['https://graph.org/file/8c7eda434e1f0baae0b81.jpg','','',''],
+        'Punarvivaha_':['https://graph.org/file/70d5850fd6e9ed8cd7bc0.jpg','','',''],
+        'Paaru_':['https://graph.org/file/f4eccb711889b4e33952a.jpg','moneycase.link','',''],
+        'Bhoomige_Bandha_Bhagavantha_':['https://graph.org/file/dc3b179fceb487e4847c4.jpg','','',''],
+        'Puttakkana_Makkalu_':['https://graph.org/file/085afbc67400a2066fefd.jpg','','',''],
+        'Gattimela_':['https://graph.org/file/d3eda0e1961ee06e5cba7.jpg','','',''],
+        'Manemagalu_':['','','',''],
+        'Sandhyaraaga_':['https://graph.org/file/98ef89a04dd66bef924ea.jpg','','',''],
+        'SeethaRaama_':['https://graph.org/file/d7b3c98a89d0d061f064f.jpg','','',''],
+        'Amruthadhaare_':['https://graph.org/file/2ebe4f1d999849a1ca574.jpg','','',''],
+        'Mahanayaka_Dr_B_R_Ambedkar_':['https://graph.org/file/6faf520d534a49f6742bb.jpg','','',''],
+        'Shrirasthu_Shubhamasthu_':['https://graph.org/file/b6d93c7cc15166fbf7e35.jpg','','',''],
+        'Soubhagyavati_Bhava_':['https://graph.org/file/2d34fb7705e6ecb7c5937.jpg','moneycase.link','',''],
+        'Hitler_Kalyana_':['https://graph.org/file/4bccb3c8fd1fe4c5d92e7.jpg','','',''],
+        'Sathya_':['https://graph.org/file/7f30ff54ecf9b380e4634.jpg','','',''],
+        'Dance_Karnataka_Dance_Season_7_':['https://graph.org/file/2d724f7a19f736c405990.jpg','','',''],
+        'Bharjari_Bachelors_':['https://graph.org/file/1b28fb7863cc31f732ac4.jpg','','',''],
+        'Couples_Kitchen_':['https://graph.org/file/fa897d25d4d944e8487d5.jpg','','',''],
+        'Chota_Champion_':['https://graph.org/file/0d77fc85da98174336f8b.jpg','','',''],
+          
+        'Pavada_Purasha_':['https://graph.org/file/f65651907ba441187f7b1.jpg','moneycase.link','',''],
+        'Olavina_Nildana_':['https://graph.org/file/441e02c5e145609e0eedd.jpg','','',''],
+        'Kendasampige_':['https://graph.org/file/62c90177d09f7cf416e6c.jpg','','',''],
+        'Bhagyalakshmi_':['https://graph.org/file/261d0c995b5e2a434093b.jpg','','',''],
+        'Ramachari_':['https://graph.org/file/f590c3585b4c0068df630.jpg','','',''],
+        'Lakshmi_Baramma_':['https://graph.org/file/be3588e70978523792514.jpg','','',''],
+        'Geetha_':['https://graph.org/file/e95baeeafbf180bd184e1.jpg','','',''],
+        'Lakshana_':['https://graph.org/file/995a7a33f5d543f4f4dbf.jpg','','',''],
+        'Punyavathi_':['https://graph.org/file/c15998a362846c9eb09f0.jpg','','',''],
+        'Tripura_Sundari_':['https://graph.org/file/4a59fd893093b82e9c234.jpg','moneycase.link','',''],
+        'Antarapata_':['https://graph.org/file/ea08c222d4848f1661e3c.jpg','','',''],
+        'Gruhapravesha_':['https://graph.org/file/16220456e337fa35b18b7.jpg','moneycase.link','',''],
+        'Ganda_Hendthi_':['https://graph.org/file/e7437d0a9eac55fdb2623.jpg','','',''],
+        'Shantham_Papam_':['https://graph.org/file/fa897d25d4d944e8487d5.jpg','','',''],
+        'Muddu_Bangara_':['https://graph.org/file/49001901d7ccbf8698935.jpg','','',''],
+        'Gunasundari_':['https://graph.org/file/8d4b6b2e9703a7d97b546.jpg','','',''],
+        'Anu_':['https://graph.org/file/7a8f5ce25159932859193.jpg','','',''],
+        'Family_Gangstars_':['https://graph.org/file/d08c55a1b259a3b5c95b7.jpg','','',''],
+
+        'Neenadhe_Naa_':['https://graph.org/file/fcbbf0a8c0413d4a59ad9.jpg','','',''],
+        'Namma_Lachi_':['https://graph.org/file/b315e59c10e04d1b2e595.jpg','','',''],
+        'Nagapanchami_':['https://graph.org/file/dd2a58d339ca517010edc.jpg','','',''],
+        'Raani_':['https://graph.org/file/5a8aed0bcd6e14604b5d8.jpg','','',''],
+        'Katheyondu_Shuruvagide_':['https://graph.org/file/e9b9f1a3dd7bc26772d5c.jpg','moneycase.link','',''],
+        'O_MuddhuManase_':['https://graph.org/file/cd2664a143bec7e2d5f14.jpg','','',''],
+        'Jenugudu_':['https://graph.org/file/48a8df66387287f6e2440.jpg','','',''],
+        'Udho_Udho_Shri_Renuka_Yellamma_':['https://graph.org/file/f5749c1343173f855b0e7.jpg','','',''],
+        'Aragini_2_':['https://graph.org/file/6ddd25521aa2c813306aa.jpg','','',''],
+        'Honganasu_':['https://graph.org/file/ca9bcfcfb5560459c3bcf.jpg','moneycase.link','',''],
+        'Anuraga_Aralitu_':['https://graph.org/file/0b15dd8c7f1d0118af62e.jpg','moneycase.link','',''],
+        'Anupama_Sarvagunaa_Sampanne_':['https://graph.org/file/0e86ebb3ab5cf24eac6c9.jpg','','',''],
+        'Yediyur_Shree_Siddhalingeshwara_':['https://graph.org/file/20ff06892579f4e3ac9ed.jpg','','',''],
+        'Bombat_Bhojana_':['https://graph.org/file/d2871411e7358e4c76b52.jpg','','',''],
+        'Suvarna_Superstar_':['https://graph.org/file/09f90f559d100a415f393.jpg','','','']
+}
