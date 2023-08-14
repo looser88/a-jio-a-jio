00fcfaeadeba5972ee4c18106faed3f7e61c1483
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-08-14 08:35:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -34,7 +34,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     elif data == "ystdy":
         DATEDAY.clear()
         ye = datetime.now(india)-timedelta(1)
-        DATEDAY.append(str(ye.strftime("%d - %m - %Y")))
+        DATEDAY.append(str(ye.strftime("%d ⚡ %m ⚡ %Y")))
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
@@ -42,7 +42,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     elif data == "tdy":
         DATEDAY.clear()
         tda = datetime.now(india)
-        DATEDAY.append(str(tda.strftime("%d - %m - %Y")))
+        DATEDAY.append(str(tda.strftime("%d ⚡ %m ⚡ %Y")))
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
@@ -50,7 +50,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     elif data == "tmr":
         DATEDAY.clear()
         tm = datetime.now(india)+timedelta(1)
-        DATEDAY.append(str(tm.strftime("%d - %m - %Y")))
+        DATEDAY.append(str(tm.strftime("%d ⚡ %m ⚡ %Y")))
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
