9a06c227cd8578e3ed2bad7cff10bfcfe7bcd316
Girish019 <118633556+Girish019@users.noreply.github.com>
2023-10-07 20:50:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -39,31 +39,33 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         except:
             pass
     elif data == "ystdy":
-        DATEDAY.clear()
+        #DATEDAY.clear()
         ye = datetime.now(india)-timedelta(1)
-        s=DATEDAY.append(str(ye.strftime("%d ⚡ %m ⚡ %Y")))
+        DATEDAY.append(str(ye.strftime("%d ⚡ %m ⚡ %Y")))
+        timeout(DATEDAY,10)
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
         			InlineKeyboardButton("Tommorow",callback_data='tmr') ]])) # A query msg edit for (in plugins->channel post->line 21) ==> this return a date from previous date stored in DATEDAY variable (line 10)
-        timeout(DATEDAY,10)
+        
     elif data == "tdy":
-        DATEDAY.clear()
+        #DATEDAY.clear()
         tda = datetime.now(india)
-        s=DATEDAY.append(str(tda.strftime("%d ⚡ %m ⚡ %Y")))
+        DATEDAY.append(str(tda.strftime("%d ⚡ %m ⚡ %Y")))
+        timeout(DATEDAY,10)
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
         			InlineKeyboardButton("Tommorow",callback_data='tmr') ]]))
-        timeout(DATEDAY,10)
+        
     elif data == "tmr":
-        DATEDAY.clear()
+        #DATEDAY.clear()
         tm = datetime.now(india)+timedelta(1)
-        s=DATEDAY.append(str(tm.strftime("%d ⚡ %m ⚡ %Y")))
+        DATEDAY.append(str(tm.strftime("%d ⚡ %m ⚡ %Y")))
+        timeout(DATEDAY,10)
         await query.message.edit_text(text = f"<b>Date change to :'{DATEDAY[-1]}'</b>", reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("Yesterday",callback_data='ystdy'), 
         			InlineKeyboardButton("Today",callback_data = 'tdy'), 
         			InlineKeyboardButton("Tommorow",callback_data='tmr') ]]))
-        timeout(DATEDAY,10)
     else:
         pass
